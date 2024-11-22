23fb8fc034df7f5275a444b8b5515d10f8bfb237
azulmouad <moad.devloper@gmail.com>
2024-11-22 13:05:55 +0100

    - product details done

--- lib/screens/product_details.dart
+++ lib/screens/product_details.dart
@@ -1,6 +1,10 @@
+import 'package:animated_rating_stars/animated_rating_stars.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:you_app/helpers/colors.dart';
 
 import '../widgets/details_w.dart';
+import '../widgets/home_w.dart';
 
 class ProductDetailsScreen extends StatefulWidget {
   const ProductDetailsScreen({super.key});
@@ -13,9 +17,199 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      backgroundColor: kPrimary,
+      bottomNavigationBar: SafeArea(
+        top: false,
+        child: Row(
+          children: [
+            CardAddToCart(
+              label: "Add to cart",
+              icon: FontAwesomeIcons.cartShopping,
+              isCart: true,
+            ),
+            const SizedBox(width: 10),
+            CardAddToCart(
+              label: "Buy now",
+              icon: FontAwesomeIcons.bagShopping,
+              isCart: false,
+            ),
+          ],
+        ),
+      ),
+      body: ListView(
+        // crossAxisAlignment: CrossAxisAlignment.start,
+        padding: EdgeInsets.zero,
         children: [
+          ///Carousel Images
           CarouselProductDetails(),
+
+          ///Title
+          Text(
+            'Full sleeve red shirt',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
+          ///Category
+          Text(
+            'Women shirt',
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 15,
+            ),
+          ),
+
+          ///Prices & Date offer
+          Row(
+            children: [
+              Text(
+                '\$10:00',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(width: 5),
+              Text(
+                '\$15:00',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(width: 10),
+              Text(
+                '20% OFF',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 14,
+                ),
+              ),
+              const Spacer(),
+              Text(
+                '16hrs : 32mins',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
+
+          ///Review
+          Row(
+            children: [
+              AnimatedRatingStars(
+                initialRating: 3.5,
+                minRating: 0.0,
+                maxRating: 5.0,
+                filledColor: Colors.amber,
+                emptyColor: Colors.grey,
+                filledIcon: Icons.star,
+                halfFilledIcon: Icons.star_half,
+                emptyIcon: Icons.star_border,
+                onChanged: (double rating) {},
+                customFilledIcon: Icons.star,
+                customHalfFilledIcon: Icons.star_half,
+                customEmptyIcon: Icons.star_border,
+                starSize: 18.0,
+                readOnly: false,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  '10 See reviews',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          ///Select Quantity
+          const Text(
+            'Select Quantity',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          ),
+          const SizedBox(height: 5),
+          CardSelectQuantity(),
+          const SizedBox(height: 10),
+
+          ///Select Size
+          const Text(
+            'Select Size: XL',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          ),
+          const SizedBox(height: 5),
+          CardSizeProduct(),
+          const SizedBox(height: 10),
+
+          ///Select Color
+          const Text(
+            'Select Color',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          ),
+          const SizedBox(height: 5),
+          const CardProductColors(),
+
+          ///Details
+          const SizedBox(height: 20),
+          CardDetailsDrop(
+            title: "Product Details",
+          ),
+          CardDetailsDrop(
+            title: "Specifications",
+          ),
+          CardDetailsDrop(
+            title: "Material & Care",
+          ),
+          CardDetailsDrop(
+            title: "Rating & Reviews",
+          ),
+
+          const SizedBox(height: 15),
+
+          ///Select Color
+          const Text(
+            'Similar Products',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          ),
+          const SizedBox(height: 10),
+          GridView(
+            shrinkWrap: true,
+            physics: ScrollPhysics(),
+            padding: EdgeInsets.zero,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              childAspectRatio: .8,
+            ),
+            children: [
+              CardProductHomeItem(),
+              CardProductHomeItem(),
+            ],
+          ),
+
+          const SizedBox(height: 100),
         ],
       ),
     );
--- lib/widgets/details_w.dart
+++ lib/widgets/details_w.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../helpers/colors.dart';
 import '../helpers/constants.dart';
 
 class CarouselProductDetails extends StatefulWidget {
@@ -108,6 +110,277 @@ class _CarouselProductDetailsState extends State<CarouselProductDetails> {
           ),
         ),
       ],
+    );
+  }
+}
+
+class CardSelectQuantity extends StatelessWidget {
+  const CardSelectQuantity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: const Color(0xffEAEAEA),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.minus,
+                  size: 15,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                child: Text(
+                  '01',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.plus,
+                  size: 15,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CardSizeItem extends StatelessWidget {
+  const CardSizeItem({super.key, required this.size, required this.selected});
+  final String size;
+  final bool selected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      margin: const EdgeInsets.only(right: 5),
+      decoration: BoxDecoration(
+        color: selected ? Colors.blue : Colors.white,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      alignment: Alignment.center,
+      child: Text(
+        size,
+        style: const TextStyle(
+          fontSize: 18,
+        ),
+      ),
+    );
+  }
+}
+
+class CardSizeProduct extends StatelessWidget {
+  const CardSizeProduct({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CardSizeItem(
+          size: "S",
+          selected: false,
+        ),
+        CardSizeItem(
+          size: "M",
+          selected: true,
+        ),
+        CardSizeItem(
+          size: "L",
+          selected: false,
+        ),
+        CardSizeItem(
+          size: "XL",
+          selected: false,
+        ),
+        CardSizeItem(
+          size: "XXL",
+          selected: false,
+        ),
+      ],
+    );
+  }
+}
+
+class CardColorItem extends StatelessWidget {
+  const CardColorItem({super.key, required this.color, required this.selected});
+  final Color color;
+  final bool selected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 30,
+      height: 30,
+      margin: const EdgeInsets.only(right: 5),
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: color,
+          border: selected
+              ? Border.all(
+                  color: Colors.white,
+                  width: 3,
+                )
+              : null),
+    );
+  }
+}
+
+class CardProductColors extends StatelessWidget {
+  const CardProductColors({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CardColorItem(
+          color: Colors.red,
+          selected: false,
+        ),
+        CardColorItem(
+          color: Colors.blue,
+          selected: true,
+        ),
+        CardColorItem(
+          color: Colors.green,
+          selected: false,
+        ),
+        CardColorItem(
+          color: Colors.orange,
+          selected: false,
+        ),
+      ],
+    );
+  }
+}
+
+class CardAddToCart extends StatelessWidget {
+  const CardAddToCart(
+      {super.key,
+      required this.label,
+      required this.icon,
+      required this.isCart});
+  final String label;
+  final IconData icon;
+  final bool isCart;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: isCart ? kPrimary : Colors.white,
+          border: Border.all(color: Colors.white, width: 2),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              color: isCart ? Colors.white : kPrimary,
+              size: 18,
+            ),
+            const SizedBox(width: 10),
+            Text(
+              label,
+              style: TextStyle(
+                color: isCart ? Colors.white : kPrimary,
+                // fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardDetailsDrop extends StatefulWidget {
+  const CardDetailsDrop({super.key, required this.title});
+  final String title;
+
+  @override
+  State<CardDetailsDrop> createState() => _CardDetailsDropState();
+}
+
+class _CardDetailsDropState extends State<CardDetailsDrop> {
+  bool isDrop = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        color: kInputBackground,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          InkWell(
+            onTap: () {
+              setState(() {
+                isDrop = !isDrop;
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    widget.title,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Icon(
+                    isDrop
+                        ? FontAwesomeIcons.chevronDown
+                        : FontAwesomeIcons.chevronRight,
+                    size: 12,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          if (isDrop) const SizedBox(height: 10),
+          if (isDrop)
+            Text(
+              "He moonlights difficult engrossed, sportsmen interested has all Devonshire difficulty gay assistance joy.For who thoroughly conviction. ",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
